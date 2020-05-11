de59ddb7813981e2dd292aab557100cdfd8706a4
ZyXeM <mitchkuijpers@hotmail.com>
2020-05-11 17:27:45 +0200

    Auth

---
+++ src/main/java/com/SpringAuthenticationServer/authenticationServer/AuthenticationServerApplication.java
@@ -0,0 +1,26 @@
+package com.SpringAuthenticationServer.authenticationServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+@EnableJpaRepositories(basePackages = {"com.SpringAuthenticationServer.authenticationServer.DataLayer"})
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.SpringAuthenticationServer.authenticationServer.Controllers"})
+@EntityScan(basePackages = "com.SpringAuthenticationServer.authenticationServer.Models")
+public class AuthenticationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServerApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+}
---
+++ src/main/java/com/SpringAuthenticationServer/authenticationServer/DataLayer/ApplicationUserRepository.java
@@ -0,0 +1,8 @@
+package com.SpringAuthenticationServer.authenticationServer.DataLayer;
+
+import com.SpringAuthenticationServer.authenticationServer.Models.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser,Long> {
+    ApplicationUser findByUsername(String username);
+}
